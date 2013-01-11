8632398309c22ea8d7a4a09db27babc320143832
Ze'ev Klapow <zklapow@gmail.com>
2013-01-11 11:11:58 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+sneeze
+=====
+
+Sneeze tranforms TODOS in your code into issues on github.
+
+Simply cd into your projects directory and run sneeze:
+
+``sneeze .``
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name="sneeze",
+    version="0.1a",
+    packages=["sneeze"],
+    long_description=open('README.md').read(),
+)
