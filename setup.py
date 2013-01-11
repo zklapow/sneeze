7170fc3092ec7cd7ce69452f77a13bc959498083
Ze'ev Klapow <zklapow@gmail.com>
2013-01-11 11:16:48 -0500

    More setup.

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(
     version="0.1a",
     packages=["sneeze"],
     long_description=open('README.md').read(),
+    author="Ze'ev Klapow",
+    author_email="zklapow@gmail.com",
+    url="http://zklapow.github.com/sneeze",
 )
